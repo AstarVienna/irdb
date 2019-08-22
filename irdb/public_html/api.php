c6001d40f79fe1cd7ad8a63cb81fdd7c69e349ff
Kieran <kdleschinski@gmail.com>
2019-08-22 14:37:19 +0200

    added web microservice to save IP information of users

---
+++ irdb/public_html/api.php
@@ -0,0 +1,27 @@
+<?php
+//start the server by running the ezphp.exe in parent folder
+//$arr = array ('REMOTE_ADDR'=>$_SERVER['REMOTE_ADDR']);
+
+    function log_package_use() {
+        $log_file = "scopesim.log";
+        $log_dict = array("timestamp"=>time(),
+                          "time"=>date("Y-m-d")."T".date("H:i:s"),
+                          "ip"=>$_SERVER['REMOTE_ADDR'],
+                          "package_name"=>$_GET['package_name']
+                          );
+
+        file_put_contents($log_file,
+                          json_encode($log_dict)."\n",
+                          FILE_APPEND | LOCK_EX
+                          );
+
+        return $log_dict;
+    }
+
+    if (array_key_exists("package_name", $_GET)) {
+        $log_dict = log_package_use();
+    }
+
+    echo json_encode($log_dict);
+
+?>
---
+++ irdb/public_html/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <title>EzPHP</title>
+  </head>
+  <body>
+	<div class="container">
+	<h1>Welcome to your personal web server</h1>
+		<p>
+		<?= "This file is located in: C:\Work\irdb\irdb\public_html\index.php"; ?>
+		</p>
+		<?= "Replaced it with your own file"; ?>
+		<p>
+		<hr>
+		<p>
+		<?= "Php version: " . phpversion() ?>
+		<p>
+	</div>
+    </body>
+</html>
