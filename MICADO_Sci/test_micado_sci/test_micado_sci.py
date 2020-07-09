ded547f7cc3cb91102983d82d70abb3882693e97
Kieran <kdleschinski@gmail.com>
2020-07-09 16:13:03 +0200

    updates to MICADO-Sci

---
+++ MICADO_Sci/test_micado_sci/test_micado_sci.py
@@ -0,0 +1,17 @@
+from os import path as pth
+import numpy as np
+import astropy.units as u
+import matplotlib.pyplot as plt
+import scopesim as sim
+
+TOP_PATH = pth.abspath(pth.join(pth.dirname(__file__), "../../"))
+
+
+def test_elt_ter_curve_reads_in():
+    filename = pth.join(TOP_PATH, "MICADO_Sci", "TER_ELT_System_20190611.dat")
+    elt = sim.effects.TERCurve(filename=filename)
+    assert isinstance(elt, sim.effects.TERCurve)
+
+    wave = np.arange(0.3, 2.5, 0.001) * u.um
+    plt.plot(wave, elt.surface.reflection(wave))
+    plt.show()
