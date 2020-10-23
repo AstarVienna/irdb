a0976e76a448f396bf8dbc37c34821e2ba0add90
Kieran <kdleschinski@gmail.com>
2020-10-23 15:34:04 +0200

    produced spec trace files for MICADO_Sci

--- MICADO_Sci/code/make_simplified_trace_files.py
+++ MICADO_Sci/code/make_simplified_trace_files.py
@@ -1,27 +1,11 @@
 from astropy.io import fits
 from astropy.table import Table
+from astropy import units as u
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def make_hdulist_fits_file():
-
-    pri_hdu = fits.PrimaryHDU()
-    meta = {"ECAT": 1,
-            "EDATA": 2}
-    pri_hdu.header.update(meta)
-
-    names = ["description", "extension_id", "aperture_id", "image_plane_id"]
-    data = [["IJ", "HK"], [2, 3], [0, 0], [0, 0]]
-    cat_tbl = Table(names=names, data=data)
-    cat_hdu = fits.table_to_hdu(cat_tbl)
-
-    names = ["wavelength", "s0", "s1", "x0", "x1", "y0", "y1"]
-    data = []
-
-    ij_tbl = None
-
-
-def make_waves_ys():
+def make_waves_and_ys():
     waves = {int(np.round(wave, 2) * 1000): None for wave in np.arange(0.60, 2.51, 0.01)}
     hdulist = fits.open("../../MICADO/TRACE_3arcsec.fits")
     for ext in hdulist[2:]:
@@ -43,11 +27,61 @@ def make_waves_ys():
             dys[i] = dys[i-1]
 
     ys = np.cumsum(np.array(dys))
-    # import matplotlib.pyplot as plt
-    # plt.plot(ws[ws>=0.78], dys[ws>=0.78])
-    # plt.show()
+
+    plt.plot(ws[ws>=0.78], dys[ws>=0.78])
 
     return ws, ys
 
 
-print(make_waves_ys())
+def make_trace_table(wave_min, wave_max, width=3):
+
+    waves, ys = make_waves_and_ys()
+    mask = (waves >= wave_min) * (waves <= wave_max)
+    waves = waves[mask] * u.um
+    ys = ys[mask] * u.mm
+
+    n = len(waves)
+    xs = np.ones(n) / 0.004 * 0.015 * u.mm
+    ss = np.ones(n) * u.arcsec
+
+    names = ["wavelength",
+             "s0", "s1",
+             "x0", "x1",
+             "y0", "y1"]
+    data = [waves,
+            -0.5 * width * ss, 0.5 * width * ss,
+            -0.5 * width * xs, 0.5 * width * xs,
+            ys, ys]
+    tbl = Table(names=names, data=data)
+
+    return tbl
+
+
+def make_hdulist_fits_file(width=3):
+    pri_hdu = fits.PrimaryHDU()
+    meta = {"ECAT": 1,
+            "EDATA": 2}
+    pri_hdu.header.update(meta)
+
+    names = ["description", "extension_id", "aperture_id", "image_plane_id"]
+    data = [["IJ", "HK"], [2, 3], [0, 0], [0, 0]]
+    cat_tbl = Table(names=names, data=data)
+    cat_hdu = fits.table_to_hdu(cat_tbl)
+    cat_hdu.header["EXTNAME"] = "CAT_TRAC"
+
+    ij_tbl = make_trace_table(0.75, 1.5, width)
+    ij_hdu = fits.table_to_hdu(ij_tbl)
+    ij_hdu.header["EXTNAME"] = "IJ_TRACE"
+
+    hk_tbl = make_trace_table(1.40, 2.5, width)
+    hk_hdu = fits.table_to_hdu(hk_tbl)
+    hk_hdu.header["EXTNAME"] = "HK_TRACE"
+
+    hdu_list = fits.HDUList([pri_hdu, cat_hdu, ij_hdu, hk_hdu])
+
+    return hdu_list
+
+
+for width in [3, 15]:
+    hdu_list = make_hdulist_fits_file(width)
+    hdu_list.writeto(f"TRACE_SCI_{width}arcsec.fits", overwrite=True)
