b6a11e3af552339cea24e863a88af091ada71ba0
Kieran <kdleschinski@gmail.com>
2021-12-07 14:24:10 +0100

    rescaled and partially reformatted psf file to avoid jumps in output spectrum

---
+++ METIS/tests/rescale_psf.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy.ndimage import zoom
+from astropy.io import fits
+
+
+def rescale_image(old_hdu, new_cdelt, max_size=512):
+    scale_factor = old_hdu.header["CDELT1"] / new_cdelt
+    image = zoom(old_hdu.data, scale_factor)
+    image /= image.sum()
+    y, x = np.divmod(np.argmax(image), image.shape[1])
+    r = max_size // 2
+    x0, x1 = x - r, x + r
+    y0, y1 = y - r, y + r
+    image = image[y0:y1, x0:x1]
+    y, x = np.divmod(np.argmax(image), image.shape[1])
+
+    new_hdu = fits.ImageHDU(data=image, header=old_hdu.header)
+    new_hdu.header["CDELT1"] = new_cdelt
+    new_hdu.header["CDELT2"] = new_cdelt
+    new_hdu.header["CRPIX1"] = x
+    new_hdu.header["CRPIX2"] = y
+    new_hdu.header["PIXSCALE"] = new_cdelt
+
+    return new_hdu
+
+
+old_hdul = fits.open("../PSF_SCAO_9mag_06seeing.fits")
+new_hdul = fits.HDUList()
+new_hdul.append(fits.PrimaryHDU())
+new_hdul.append(rescale_image(old_hdul[2], 5.47))
+new_hdul.append(rescale_image(old_hdul[3], 6.79))
+new_hdul.append(rescale_image(old_hdul[4], 6.79))
+new_hdul.append(rescale_image(old_hdul[5], 6.79))
+
+for i in range(1, 5):
+    new_hdul[0].header[f"WAVE{i}"] = new_hdul[i].header["WAVELENG"], "um"
+
+old_keys = {"FILETYPE": "Point Spread Functions",
+            "AUTHOR"  : "Oliver Czoske",
+            "DATE"    : "2018-10-09",
+            "SOURCE"  : "Markus Feldt, MPIA",
+            "ORIGDATE": "2018-??-??"}
+new_hdul[0].header.update(old_keys)
+
+for history in ["CHANGES:",
+                "- 2018-10-09: [OC] Formatted for ScopeSim",
+                "- 2021-12-07: [KL] Rescaled to METIS detector pixel scales"]:
+    new_hdul[0].header["HISTORY"] = history
+
+new_hdul.writeto("PSF_SCAO_9mag_06seeing_rescaled.fits")
--- METIS/tests/test_ScopeSim_MET_LSS.py
+++ METIS/tests/test_ScopeSim_MET_LSS.py
@@ -33,6 +33,7 @@ from astropy import units as u
 
 import scopesim as sim
 from scopesim.source.source_templates import star, empty_sky
+from scopesim.source import source_templates as st
 from scopesim import rc
 
 import hmbp
@@ -148,4 +149,153 @@ class TestMetisLss:
         metis = sim.OpticalTrain(cmds)
         print(metis.effects)
 
+    def test_slit_losses(self):
+        src = star(flux=1 * u.Jy)
+
+        cmds_lss = sim.UserCommands(use_instrument="METIS", set_modes=["lss_l"])
+        metis_lss = sim.OpticalTrain(cmds_lss)
+        metis_lss["lss_spectral_traces"].include = False
+        metis_lss["armazones_atmo_skycalc_ter_curve"].include = False
+        metis_lss["eso_combined_reflection"].include = False
+        metis_lss["metis_cfo_surfaces"].include = False
+
+        metis_lss.observe(src)
+
+        cmds_img = sim.UserCommands(use_instrument="METIS", set_modes=["lss_l"])
+        metis_img = sim.OpticalTrain(cmds_img)
+        metis_img["lss_spectral_traces"].include = False
+        metis_img["armazones_atmo_skycalc_ter_curve"].include = False
+        metis_img["eso_combined_reflection"].include = False
+        metis_img["metis_cfo_surfaces"].include = False
+        metis_img["slit_wheel"].include = False
+
+        metis_img.observe(src)
+
+        n = 32
+        img_slit = metis_lss.image_planes[0].data[1024-n:1024+n, 1024-n:1024+n]
+        img_normal = metis_img.image_planes[0].data[1024-n:1024+n, 1024-n:1024+n]
+
+        plt.subplot(211)
+        plt.imshow(img_slit, origin="lower", norm=LogNorm(), vmin=1e-8)
+        plt.title(img_slit.sum())
+
+        plt.subplot(212)
+        plt.imshow(img_normal, origin="lower", norm=LogNorm(), vmin=1e-8)
+        plt.title(img_normal.sum())
+
+        plt.show()
+
+    def test_delta_function_psf_for_random_spectroscopy_jumps(self):
+        sim_src = sim.source.source_templates.star(flux=0 * u.mag)
+
+        cmd = sim.UserCommands(use_instrument="METIS", set_modes=["lss_m"])
+        metis = sim.OpticalTrain(cmd)
+        # metis["metis_psf_img"]._file[1].data = metis["metis_psf_img"]._file[2].data
+
+        # kernel = np.zeros([7, 7])
+        # kernel[1:6, 1:6] = 0.05
+        # kernel[2:5, 2:5] = 0.1
+        # kernel[3, 3] = 0.5
+        # metis["metis_psf_img"]._file[1].data = kernel
+        # metis["metis_psf_img"]._file[2].data = kernel
+        #
+        # for i, fov in enumerate(metis.fov_manager.fovs):
+        #     psf = metis["metis_psf_img"].get_kernel(fov)
+        #     plt.subplot(1, 2, i+1)
+        #     plt.imshow(psf)
+        #     plt.title(f"{psf.sum()}")
+        #     plt.colorbar()
+        # plt.show()
+
+        metis.observe(sim_src)
+        img_src = metis.image_planes[0].data[:, 1016:1024]
+        img_bg = metis.image_planes[0].data[:, 816:824]
+
+        plt.semilogy(np.sum(img_src, axis=1))
+        plt.semilogy(np.sum(img_bg, axis=1))
+        plt.show()
+
+    def test_HL_tau(self):
+        from astropy.io import fits
+        hdul = fits.open(r"F:\temp\scopesim_metis_workshop\HL_Tau_prep_for_Scopesim.fits")
+        src = sim.Source(image_hdu=hdul[1], flux=1*u.MJy)
+
+        cmd = sim.UserCommands(use_instrument='METIS', set_modes=['img_n'])
+        metis = sim.OpticalTrain(cmd)
+        metis['detector_linearity'].include = False
+
+        metis.observe(src, update=True)
+
+        img = metis.image_planes[0].data
+        img = metis.readout()[0][1].data
+
+        plt.imshow(img)
+        plt.show()
+
+    def test_roys_cube(self):
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from matplotlib.colors import LogNorm
+
+        from astropy import units as u
+        from astropy.io import fits
+
+        import scopesim as sim
+
+        sim.rc.__currsys__["!SIM.file.local_packages_path"] = "F:/Work/irdb"
+
+        fname = r"F:\temp\scopesim_metis_workshop\models_Lband_HD100546_gap100.cube_rebinned.fits"
+        hdulist = fits.open(fname)
+
+        hdr = hdulist[0].header
+        hdr_info = {"CDELT1": hdr["PFOV"],
+                    "CDELT2": hdr["PFOV"],
+                    "CDELT3": 0.02,
+                    "CUNIT3": "um",
+                    "CRPIX3": 0,
+                    "CRVAL3": 3.10,
+                    "CTYPE3": "WAVE",
+                    "CUNIT1": "arcsec",
+                    "CUNIT2": "arcsec",
+                    "BUNIT": "Jy",
+                    }
+        hdulist[0].header.update(hdr_info)
+
+        src = sim.Source(image_hdu=hdulist[0], flux=1 * u.Jy)
+
+        cmd = sim.UserCommands(use_instrument="METIS", set_modes=["lss_l"])
+        metis = sim.OpticalTrain(cmd)
+
+        metis.observe(src)
+
+        img = metis.image_planes[0].data
+
+        plt.imshow(img)
+        plt.show()
+
+    def test_compare_anisocado_psf_to_metis_psf(self):
+        import anisocado
+        psf = anisocado.AnalyticalScaoPsf(N=512, wavelength=3.8,
+                                          pixelSize=0.00765457,
+                                          profile_name="EsoQ1")
+        psf_an = psf.make_psf()
+        psf_an /= psf_an.sum()
+
+        from astropy.io import fits
+        psf_im = fits.getdata("../PSF_SCAO_9mag_06seeing.fits", ext=2)[256:768, 256:768]
+        psf_im /= psf_im.sum()
+
+        plt.subplot(131)
+        plt.imshow(psf_an, norm=LogNorm(vmin=1e-7))
+        plt.colorbar()
+
+        plt.subplot(132)
+        plt.imshow(psf_im, norm=LogNorm(vmin=1e-7))
+        plt.colorbar()
+
+        plt.subplot(133)
+        plt.imshow(np.abs(psf_im / psf_an - 1), vmax=1)
+        plt.colorbar()
+
+        plt.show()
 
