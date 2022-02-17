2de3fe3855be574c001d785e843162616946dd2e
Kieran <kdleschinski@gmail.com>
2022-02-17 08:56:42 +0100

    started with auto-docs for package effect descriptions

--- docs/code/generate_rst_pipeline.py
+++ docs/code/generate_rst_pipeline.py
@@ -1,4 +1,7 @@
+import os
 import os.path as pth
+import yaml
+
 import scopesim as sim
 from scopesim import rc
 from scopesim.utils import write_report
@@ -87,5 +90,63 @@ def make_micado_sci_rst_files():
     summary_effects(opt_man, filename="sci_summary.rst")
 
 
-make_micado_rst_files()
+def make_package_report_files():
+    # make "packages" folder in docs/source
+    # make <package name> folder in docs/source/packages
+    # make <package name> folder in docs/source/_static
+    # open irdb/package.yaml
+    # loop though the packages
+    # set the !SIM.reports.rst_path and images_path
+    # if package has a no default.yaml,
+    # - create a single OpticalElement and generate report
+    # else
+    # - loop through the yamls, and exclude names that appear in packages.yaml
+    # - loop through the mode_yamls, include any extra properties
+
+    top_dir = "../../"
+    source_packages = "../source/packages"
+    source_static = "../source/_static"
+
+    with open(pth.join(top_dir, "irdb/packages.yaml")) as f:
+        import yaml
+        pkg_names = list(yaml.full_load(f).keys())
+
+    for pkg_name in pkg_names[:1]:
+        orig_pkg_path = pth.join(top_dir, pkg_name)
+        rst_save_path = pth.join(source_packages, pkg_name)
+        image_save_path = pth.join(source_static, pkg_name)
+
+        if not pth.exists(rst_save_path):
+            os.mkdir(rst_save_path)
+        if not pth.exists(image_save_path):
+            os.mkdir(image_save_path)
+
+        rc.__currsys__["!SIM.reports.rst_path"] = rst_save_path
+        rc.__currsys__["!SIM.reports.image_path"] = image_save_path
+
+        if "../" not in rc.__search_path__:
+            rc.__search_path__.insert(0, "../")
+
+        default_yaml = pth.join(orig_pkg_path, "default.yaml")
+        if not pth.exists(default_yaml):
+            default_yaml = pth.join(orig_pkg_path, "_default_standalone.yaml")
+
+        cmd = sim.UserCommands(yamls=[default_yaml])
+        rc.__currsys__ = cmd
+
+        opt_els = []
+        for yaml in cmd.yaml_dicts:
+            opt_el = sim.optics.OpticalElement(yaml_dict=yaml)
+            opt_els += [opt_el]
+            fname = pth.join(rc.__config__["!SIM.reports.rst_path"],
+                             "sci_{}.rst".format(opt_el.meta["name"]))
+            opt_el.report(filename=fname)
+
+
+
+
+
+
+# make_micado_rst_files()
 # make_micado_sci_rst_files()
+make_package_report_files()
