6cc1ac5da7b8990f0df5f7f88539c4941dc46313
Kieran <kdleschinski@gmail.com>
2022-03-12 17:20:46 +0100

    [MAAT] yamls now working for OSIRIS Imager mode

--- OSIRIS/tests/test_compiles.py
+++ OSIRIS/tests/test_compiles.py
@@ -1,7 +1,10 @@
 import os
+import numpy as np
 
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
+
+from astropy import units as u
 
 from scopesim.source.source_templates import star_field
 import scopesim as sim
@@ -24,9 +27,21 @@ class TestOsirisCompiles:
 
         assert isinstance(osiris, sim.OpticalTrain)
 
+    def test_system_throughput_is_not_zero(self):
+        cmds = sim.UserCommands(use_instrument="OSIRIS")
+        osiris = sim.OpticalTrain(cmds)
+        wave = np.arange(0.3, 1.0, 0.001) * u.um
+        trans = osiris.optics_manager.system_transmission(wave)
+
+        if PLOTS:
+            plt.plot(wave, trans)
+            plt.show()
+
+        assert trans.sum() > 0
+
     def test_run_simulation(self):
         # n stars, mag_min, mag_max, width=[arcsec]
-        src = star_field(n=400, mmin=10, mmax=20, width=60)
+        src = star_field(n=1000, mmin=10, mmax=20, width=450, use_grid=False)
 
         cmds = sim.UserCommands(use_instrument="OSIRIS")
         osiris = sim.OpticalTrain(cmds)
@@ -35,4 +50,5 @@ class TestOsirisCompiles:
 
         if PLOTS:
             plt.imshow(hdulist[1].data, norm=LogNorm())
-            plt.imshow()
+            plt.colorbar()
+            plt.show()
