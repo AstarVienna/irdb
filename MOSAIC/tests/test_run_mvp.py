2f0be2622c8e91c761d20729567228921284d12e
Kieran Leschinski <kdleschinski@gmail.com>
2023-04-24 10:20:50 +0200

    radiometry test for mosaic

--- MOSAIC/tests/test_run_mvp.py
+++ MOSAIC/tests/test_run_mvp.py
@@ -2,6 +2,9 @@ import pytest
 from os import path as pth
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+import numpy as np
+
+from astropy import units as u
 
 import scopesim as sim
 import scopesim.source.source_templates as st
@@ -33,8 +36,54 @@ class TestMosiacMvpCanObserveSomething:
         mosaic.cmds["!ATMO.temperature"] = 0
 
         mosaic.observe(src)
+        hdul = mosaic.readout()[0]
+
+        im = mosaic.image_planes[0].data
+        im2 = hdul[1].data
 
         if PLOTS:
-            plt.imshow(mosaic.image_planes[0].data, norm=LogNorm(), origin="lower")
+            plt.figure(figsize=(13, 6))
+            plt.subplot(121)
+            plt.imshow(im, norm=LogNorm(), origin="lower")
+            plt.subplot(122)
+            plt.imshow(im2, origin="lower")
             plt.pause(0)
             plt.show()
+
+        # assert im[:, :].sum()
+
+    def test_run_observe_with_star(self):
+        src = st.star(0, 0, 20*u.mag)
+        wave = np.linspace(1.420, 1.825, 4096) * u.um
+
+        cmds = sim.UserCommands(use_instrument="MOSAIC")
+        mosaic = sim.OpticalTrain(cmds)
+        mosaic.cmds["!ATMO.temperature"] = 0
+
+        mosaic.observe(src)
+        hdul = mosaic.readout()[0]
+
+        in_flux = (np.trapz(src.spectra[0](wave), wave) *
+                   src.fields[0]["weight"] *
+                   (mosaic.cmds["!TEL.area"] *
+                    mosaic.cmds["!OBS.dit"] * u.s)).to(u.ph).value
+        out_flux = np.sum(mosaic._last_fovs[0].hdu.data)
+
+        assert 0.3 < out_flux / in_flux < 0.5
+
+        im = mosaic.image_planes[0].data
+        im2 = hdul[1].data
+
+        if PLOTS:
+            plt.figure(figsize=(13, 6))
+            plt.subplot(121)
+            plt.imshow(im, norm=LogNorm(), origin="lower")
+            plt.subplot(122)
+            plt.imshow(im2, origin="lower")
+            plt.pause(0)
+            plt.show()
+
+
+class TestRadiometryOfMvp:
+    def test_same_number_of_photons_go_in_and_out(self):
+        pass
