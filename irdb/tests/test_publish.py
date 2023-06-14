064b0f152f242fca1b06c91d9139cd0a1b2c5296
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-06-14 12:40:32 +0200

    Make sure tests do not modify local files

--- irdb/tests/test_publish.py
+++ irdb/tests/test_publish.py
@@ -1,42 +1,67 @@
+import copy
 import os
 from os import path as pth
 from datetime import datetime as dt
 
 import pytest
+import yaml
 
 from .. import publish as pub
+
+
+PATH_TEST_PACKAGE_VERSION_YAML = pub.PKGS_DIR / "test_package" / "version.yaml"
+NOW = dt.now().strftime("%Y-%m-%d")
 
 
 @pytest.fixture(name="preserve_versions", scope="function")
 def fixture_preserve_versions():
     """Preserve the versions of test packages."""
-    p_test_package = pub.PKGS_DIR / "test_package" / "version.yaml"
 
     # Backup version information.
     b_yaml_packages = pub.PATH_PACKAGES_YAML.read_bytes()
-    b_yaml_test_package = p_test_package.read_bytes()
+    b_yaml_test_package = PATH_TEST_PACKAGE_VERSION_YAML.read_bytes()
+    b_pkgs = copy.deepcopy(pub.PKGS)
 
     # yield instead of return so the fixture can clean up afterwards
     yield
 
     # Put the original values back.
     pub.PATH_PACKAGES_YAML.write_bytes(b_yaml_packages)
-    p_test_package.write_bytes(b_yaml_test_package)
+    PATH_TEST_PACKAGE_VERSION_YAML.write_bytes(b_yaml_test_package)
+    pub.PKGS = b_pkgs
 
 
 def test_make_packages(preserve_versions):
     os.chdir(pth.join(pth.dirname(__file__), "../../"))
     pkg_name = "test_package"
-    now = str(dt.now())[:10]
     zip_name = pub.make_package(pkg_name, release="dev")
 
-    assert zip_name == f"{pkg_name}.{now}.dev"
+    assert zip_name == f"{pkg_name}.{NOW}.dev"
     assert pth.exists(pth.join("_ZIPPED_PACKAGES", zip_name+".zip"))
+
+    version_dict = yaml.safe_load(open(PATH_TEST_PACKAGE_VERSION_YAML))
+    assert version_dict["version"] == f"{NOW}.dev"
 
 
 def test_run_main(preserve_versions):
     argv = ["", "-c", "test_package", "-p", "<insert_if_running_locally"]
     pub.main(argv)
+    with open(PATH_TEST_PACKAGE_VERSION_YAML, encoding="utf8") as f:
+        version_dict = yaml.safe_load(f)
+    assert version_dict["version"] == NOW
+
+
+def test_run_main_no_update_version():
+    argv = ["", "-c", "test_package", "-p", "<insert_if_running_locally", "-d"]
+    pub.main(argv)
+
+    with open(PATH_TEST_PACKAGE_VERSION_YAML, encoding="utf8") as f:
+        version_dict = yaml.safe_load(f)
+    assert version_dict["version"] == "2022-07-11.dev"
+
+    with open(pub.PATH_PACKAGES_YAML, encoding="utf8") as f:
+        pckgs_dict = yaml.safe_load(f)
+    assert pckgs_dict["test_package"]["stable"] == "test_package.2022-07-11"
 
 
 # def rename_zips():
