6524ec8d1dd71b62829f5ebb2682772f88d74d22
teutoburg <ghost@instruct.at>
2023-07-31 15:15:41 +0200

    Properly do parametrisation

--- irdb/tests/test_package_contents.py
+++ irdb/tests/test_package_contents.py
@@ -12,125 +12,138 @@ from astropy.io.ascii import InconsistentTableError
 from irdb.utils import get_packages, recursive_filename_search
 from irdb.badges import BadgeReport
 
-PKG_DIR = Path(__file__).parent.parent.parent
-PKG_DICT = dict(get_packages())
+
+@pytest.fixture(scope="module")
+def pkg_dir():
+    return Path(__file__).parent.parent.parent
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="module")
 def badges():
     with BadgeReport() as report:
         yield report
 
 
 class TestFileStructureOfPackages:
+    @pytest.mark.parametrize("package", list(get_packages()))
     @pytest.mark.usefixtures("badges")
-    def test_default_yaml_contains_packages_list(self, badges):
-        bad_packages = []
-        for pkg_name, pkg_path in PKG_DICT.items():
-            default_yaml = pkg_path / "default.yaml"
-            if not default_yaml.exists():
-                badges[f"!{pkg_name}.package_type"] = "support"
-                continue
+    def test_default_yaml_contains_packages_list(self, package, badges):
+        pkg_name, pkg_path = package
+        default_yaml = pkg_path / "default.yaml"
+        if not default_yaml.exists():
+            badges[f"!{pkg_name}.package_type"] = "support"
+            return
 
-            badges[f"!{pkg_name}.package_type"] = "observation"
+        badges[f"!{pkg_name}.package_type"] = "observation"
 
-            with default_yaml.open(encoding="utf-8") as file:
-                yaml_dict = next(yaml.full_load_all(file))
+        with default_yaml.open(encoding="utf-8") as file:
+            yaml_dict = next(yaml.full_load_all(file))
 
-            result = "packages" in yaml_dict and "yamls" in yaml_dict and \
-                     f"{pkg_name}.yaml" in yaml_dict["yamls"]
-            if result:
-                badges[f"!{pkg_name}.structure.default_yaml"] = "OK"
-            else:
-                badges[f"!{pkg_name}.structure.default_yaml"] = "incomplete"
-                bad_packages.append(pkg_name)
-        assert not bad_packages
+        result = "packages" in yaml_dict and "yamls" in yaml_dict and \
+                 f"{pkg_name}.yaml" in yaml_dict["yamls"]
+        if result:
+            badges[f"!{pkg_name}.structure.default_yaml"] = "OK"
+        else:
+            badges[f"!{pkg_name}.structure.default_yaml"] = "incomplete"
+            assert result, pkg_name
 
+    @pytest.mark.parametrize("package", list(get_packages()))
     @pytest.mark.usefixtures("badges")
-    def test_all_packages_have_a_self_named_yaml(self, badges):
+    def test_all_packages_have_a_self_named_yaml(self, package, badges):
         """This test can never fail.
 
         get_packages() decides whether a directory is a package based on
         whether it has a yaml file in it with the same name.
         """
-        bad_packages = []
-        for pkg_name, pkg_path in PKG_DICT.items():
-            if (pkg_path / f"{pkg_name}.yaml").exists():
-                badges[f"!{pkg_name}.structure.self_named_yaml"] = "found"
-            else:
-                badges[f"!{pkg_name}.structure.self_named_yaml"] = "not found"
-                bad_packages.append(pkg_name)
-        assert not bad_packages
+        pkg_name, pkg_path = package
+        if (result := (pkg_path / f"{pkg_name}.yaml").exists()):
+            badges[f"!{pkg_name}.structure.self_named_yaml"] = "found"
+        else:
+            badges[f"!{pkg_name}.structure.self_named_yaml"] = "not found"
+        assert result, pkg_name
 
     @pytest.mark.xfail(
-        reason="Most of the missing files seem to exist in other packages though, so they are probably okay."
+        reason=("Most of the missing files seem to exist in other packages "
+                "though, so they are probably okay.")
     )
+    @pytest.mark.parametrize("package", list(get_packages()))
     @pytest.mark.usefixtures("badges")
-    def test_all_files_referenced_in_yamls_exist(self, badges):
+    def test_all_files_referenced_in_yamls_exist(self, package, badges):
         missing_files = []
-        for pkg_name, pkg_path in PKG_DICT.items():
+        pkg_name, pkg_path = package
+        for yaml_file in pkg_path.glob("*.yaml"):
+            with yaml_file.open(encoding="utf-8") as file:
+                # An error here shouldn't pass silently. If this regularly
+                # produces [the same] error(s), *that* error(s) should be
+                # caught and handled accordingly!
+                # Additionally: should test_all_yaml_files_readable assert
+                # that these files don't produce any errors? Hmmm
+                # try:
+                yaml_dicts = list(yaml.full_load_all(file))
+                # except Exception:
+                #     yaml_dicts = []
 
-            num_missing = 0
-            for yaml_file in pkg_path.glob("*.yaml"):
-                with yaml_file.open(encoding="utf-8") as file:
-                    try:
-                        yaml_dicts = list(yaml.full_load_all(file))
-                    except Exception:
-                        yaml_dicts = []
+            fnames = []
+            for yaml_dict in yaml_dicts:
+                fnames.extend(recursive_filename_search(yaml_dict))
 
-                fnames = []
-                for yaml_dict in yaml_dicts:
-                    fnames.extend(recursive_filename_search(yaml_dict))
+            for fname in fnames:
+                if fname is None:
+                    continue
+                if not isinstance(fname, (list, tuple)):
+                    fname = [fname]
+                for fn in fname:
+                    if fn.lower() != "none" and not fn.startswith("!"):
+                        full_fname = pkg_path / fn
+                        if not full_fname.exists():
+                            badges[pkg_name]["structure"][fn] = "missing"
+                            # Since with the parametrisation, we only have one
+                            # missing_files list per pkg, don't need full name.
+                            # missing_files.append(str(full_fname))
+                            missing_files.append(fn)
 
-                for fname in fnames:
-                    if fname is None:
-                        continue
-                    if not isinstance(fname, (list, tuple)):
-                        fname = [fname]
-                    for fn in fname:
-                        if fn.lower() != "none" and not fn.startswith("!"):
-                            full_fname = pkg_path / fn
-                            if not full_fname.exists():
-                                badges[pkg_name]["structure"][fn] = "missing"
-                                num_missing += 1
-                                missing_files.append(str(full_fname))
+        if not missing_files:
+            badges[f"!{pkg_name}.structure.no_missing_files"] = "!OK"
+        assert not missing_files, f"{pkg_name}: {missing_files=}"
 
-            if not num_missing:
-                badges[f"!{pkg_name}.structure.no_missing_files"] = "!OK"
-        assert not missing_files, f"{missing_files}"
-
+    @pytest.mark.parametrize("package", list(get_packages()))
     @pytest.mark.usefixtures("badges")
-    def test_all_yaml_files_readable(self, badges):
+    def test_all_yaml_files_readable(self, package, badges):
         yamls_bad = []
-        for pkg_name, pkg_path in PKG_DICT.items():
-            num_errors = 0
-            for yaml_file in pkg_path.glob("*.yaml"):
-                with yaml_file.open(encoding="utf-8") as file:
-                    try:
-                        _ = list(yaml.full_load_all(file))
-                    except Exception:
-                        num_errors += 1
-                        yamls_bad.append(str(yaml_file))
-                        badges[f"!{pkg_name}.contents"][yaml_file.name] = "error"
+        pkg_name, pkg_path = package
+        for yaml_file in pkg_path.glob("*.yaml"):
+            with yaml_file.open(encoding="utf-8") as file:
+                try:
+                    _ = list(yaml.full_load_all(file))
+                except Exception:
+                    # TODO: maybe Exception is too general? This used to be a
+                    #       bare except anyway...
+                    # TODO: maybe record *what* error was produced here,
+                    #       similar to how we do in test_all_dat_files_readable
+                    yamls_bad.append(str(yaml_file))
+                    badges[f"!{pkg_name}.contents"][yaml_file.name] = "error"
 
-            if not num_errors:
-                badges[f"!{pkg_name}.contents.all_yamls_readable"] = "!OK"
-        assert not yamls_bad, f"Errors found in yaml files: {yamls_bad}"
+        if not yamls_bad:
+            badges[f"!{pkg_name}.contents.all_yamls_readable"] = "!OK"
+        assert not yamls_bad, f"Errors in {pkg_name} yaml files: {yamls_bad}"
 
-    @pytest.mark.xfail
-    @pytest.mark.usefixtures("badges")
-    def test_all_dat_files_readable(self, badges):
+    @pytest.mark.xfail(
+        reason=("Due to bad globbing, files in subfolders were not checked "
+                "previously, some of those now fail. idk y tho")
+    )
+    @pytest.mark.usefixtures("pkg_dir", "badges")
+    def test_all_dat_files_readable(self, pkg_dir, badges):
         bad_files = []
         how_bad = {"inconsistent_table_error": [],
                    "value_error": [],
                    "unexpected_error": []}
-        fns_dat = PKG_DIR.rglob("*.dat")
+        fns_dat = pkg_dir.rglob("*.dat")
         # TODO: the following assert now always passed because fns_dat is a
         #       generator object (while the check was likely meant to catch
         #       empty lists)
         assert fns_dat
         for fn_dat in fns_dat:
-            fn_loc = fn_dat.relative_to(PKG_DIR)
+            fn_loc = fn_dat.relative_to(pkg_dir)
             try:
                 # FIXME: DataContainer should be updated to support Path objects...
                 _ = DataContainer(str(fn_dat))
