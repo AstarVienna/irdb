fix aftermath of off-by-one changes

MICADO/test_micado/test_micado_imaging.py
@@ -81,10 +81,9 @@ class TestLimiting:
             plt.subplot(132)
             plt.imshow(det, norm=LogNorm(vmin=np.median(det), vmax=1.01*np.median(det)))
 
-        offset = 2      # this needs to be addressed
         xpix, ypix = src.fields[0]["x"].data, src.fields[0]["y"].data
-        xpix = xpix / 0.004 + 512 + offset
-        ypix = ypix / 0.004 + 512 + offset
+        xpix = xpix / 0.004 + 512
+        ypix = ypix / 0.004 + 512
         mags = np.round(np.linspace(mmin, mmax, n_stars), 1)
 
         snrs = []

WFC3/test_wfc3/test_full_package_wfc3_ir.py
@@ -121,8 +121,10 @@ class TestMakeOpticalTrain:
             plt.show()
 
         # test that the ImagePlane is large enough
-        assert opt.image_planes[0].header["NAXIS1"] > 1024
-        assert opt.image_planes[0].header["NAXIS2"] > 1024
+        # Note: This was "> 1024", which failed after the off-by-one fix.
+        #       Is there any physical reason why 1024 exactly is not enough?
+        assert opt.image_planes[0].header["NAXIS1"] >= 1024
+        assert opt.image_planes[0].header["NAXIS2"] >= 1024
         assert np.all(opt.image_planes[0].data == 0)
 
         # test assert there are 1 detector