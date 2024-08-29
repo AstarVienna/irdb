41cb582972cf303053aff76a01d5cda46da46b02
Astrogirlanjali <u7516750@anu.edu.au>
2024-08-29 12:39:16 +1000

    Added S0, elliptical galaxy

--- DREAMS/Test_codes/Galaxy.py
+++ DREAMS/Test_codes/Galaxy.py
@@ -1,4 +1,4 @@
-#cluster in the LMC
+# Extragalatic
 import os
 import pytest
 import numpy as np
@@ -9,8 +9,7 @@ import scopesim
 import synphot
 from scopesim import Source
 from astropy.io import fits
-
-
+from scopesim_templates.extragalactic import galaxy
 from scopesim import rc
 from scopesim.source.source_templates import star_field
 import scopesim_templates as sim_tp
@@ -53,23 +52,37 @@ dreams.fov_manager.volumes_list[0]["y_max"] = 18000
 dreams.fov_manager._fovs_list = list(dreams.fov_manager.generate_fovs_list())
 
 print("scopesim package loaded successfully.")
-src = sim_tp.stellar.clusters.cluster(mass=10000,  distance=1800, core_radius=500, seed=9002)
+from scopesim_templates.extragalactic import galaxy
+src = galaxy("kc96/s0", z=1.5, amplitude=17, filter_curve="J", pixel_scale=0.01, r_eff=2.5, n=4, ellip=0.5, theta=45, extend=3)
 
 dreams.observe(src, update=False)
 print("yessss anjali")
 hdus = dreams.readout()
-#dreams.readout(filename="Han.fits")
-plt.subplot(121)
-wave = np.arange(3000, 11000)
-plt.plot(wave, dreams.optics_manager.surfaces_table.throughput(wave))
+dreams.readout(filename="gal.fits")
+# Observe the source
+dreams.observe(src, update=False)
+
+# Readout and plot
+hdus = dreams.readout()
+
 plt.subplot(122)
 im = hdus[0][1].data
-# detector_order = [2, 1, 4, 3, 6, 5]
-detector_order = [1, 2, 3, 4, 5, 6]
+plt.imshow(im, norm=LogNorm())
+plt.colorbar()
+plt.title("Observed Galaxy Field")
+plt.xlabel("X Pixels")
+plt.ylabel("Y Pixels")
+
+detector_order = [2, 1, 4, 3, 6, 5]
 plt.figure(figsize=(20, 20))
 for plot_number, hdu_number in enumerate(detector_order, 1):
     plt.subplot(3, 2, plot_number)
-    plt.imshow(hdus[0][hdu_number].data, norm=LogNorm(), cmap="hot")
+    plt.imshow(np.log10(src.fields[0].data), origin="lower")
     plt.colorbar()
-    
+    plt.title(f"HDU {hdu_number}")
+
 plt.show()
+
+
+
+
