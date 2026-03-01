301e88435a0bc91b23432e7e940cc884ca61d112
teutoburg <ghost@instruct.at>
2026-03-01 23:20:22 +0100

    Try letting test cases fail

--- MICADO/test_micado/test_micado_imaging.py
+++ MICADO/test_micado/test_micado_imaging.py
@@ -67,15 +67,15 @@ class TestLimiting:
         ("fw1", "fw2", "r0", "rics_lim_mag", "abslim"), [
             pytest.param(
                 "J", "open", 1, 27.9, 0.3,
-                marks=pytest.mark.xfail(
-                    reason="something changed in ScopeSim..."
-                    ),
+                # marks=pytest.mark.xfail(
+                #     reason="something changed in ScopeSim..."
+                #     ),
                 ),
             pytest.param(
                 "open", "H", 2, 27.5, 0.3,
-                marks=pytest.mark.xfail(
-                    reason="something changed in ScopeSim..."
-                    ),
+                # marks=pytest.mark.xfail(
+                #     reason="something changed in ScopeSim..."
+                #     ),
                 ),
             ("open", "Ks", 2, 27.1, 0.3),
         ],
